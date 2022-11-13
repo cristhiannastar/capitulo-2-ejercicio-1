02355c71ecd324116787ed21ea76b25b372aef6b
Hp <Hp@DESKTOP-NVV8KB0>
2022-11-13 11:31:58 -0500

    Agregar archivos de proyecto.

---
+++ capitulo 2 ejercicio 1/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ejercicio1
+{
+
+    class program
+    {
+        public static void Main()
+        {
+            int contador;
+            int limite;
+            int sumatoria;
+            sumatoria = 0;
+            contador = 1;
+            limite = 10;
+            for (contador =1; contador<= limite; contador++)
+            {
+
+                sumatoria = sumatoria + contador;
+
+                Console.WriteLine($"El resultado de la sumatoria de los numeros es:{sumatoria}");
+            }
+
+        }
+    }
+
+
+}
